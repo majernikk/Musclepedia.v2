5c92820725516f315728947de4c01e7d91724b38
majernik <kikomajernik@gmail.com>
2022-04-20 22:48:51 +0200

    coom

--- app/src/main/java/sk/spse/musclepediav2/Activities/ActivityAbdominals.java
+++ app/src/main/java/sk/spse/musclepediav2/Activities/ActivityAbdominals.java
@@ -4,17 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import sk.spse.musclepediav2.R;
 
 public class ActivityAbdominals extends AppCompatActivity {
 
+    private TextView nazov;
+    private TextView cvik1;
+    private TextView cvik2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity);
+        TextView nazov=(TextView)findViewById(R.id.nazov);
+        TextView cvik1=(TextView)findViewById(R.id.cvik1);
+        TextView cvik2=(TextView)findViewById(R.id.cvik2);
+        nazov.setText("Abdominals");
+        cvik1.setText("Crunches");
+        cvik2.setText("Forearm plank");
+        Toast.makeText(getApplicationContext(), "We are moved to Activity",Toast.LENGTH_LONG).show();
 
-        TextView textView = new TextView(this);
-        textView.setText("Abdominals");
     }
 }
